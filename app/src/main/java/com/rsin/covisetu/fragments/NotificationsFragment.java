789b345d578b1636a59d0e38f412e2557b60eb30
Rahul <rahulsingh.83748@gmail.com>
2021-05-28 16:05:38 +0530

    Initial commit

---
+++ app/src/main/java/com/rsin/covisetu/fragments/NotificationsFragment.java
@@ -0,0 +1,29 @@
+package com.rsin.covisetu.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.rsin.covisetu.R;
+
+
+public class NotificationsFragment extends Fragment {
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
+        final TextView textView = root.findViewById(R.id.text_notifications);
+        //code here
+        return root;
+    }
+}
